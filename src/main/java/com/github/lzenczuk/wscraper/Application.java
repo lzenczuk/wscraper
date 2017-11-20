978aaaa2ed4e801bccc16078d560dbd28e6fbbbb
Lucjan Zenczuk <lucjan.zenczuk@gmail.com>
2017-11-20 15:48:48 +0000

    Init app with minimum maven and spring boot code.

---
+++ src/main/java/com/github/lzenczuk/wscraper/Application.java
@@ -0,0 +1,25 @@
+package com.github.lzenczuk.wscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by dev on 20/11/17.
+ */
+@Controller
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
